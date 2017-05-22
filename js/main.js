a54e3ef0fa44d50c92783632be8ebf22c8b7d0f9
karina palacios <karina.spalacios@gmail.com>
2017-05-22 12:17:22 -0400

    asignando css

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//Trayendonos los elementos que utilizaremos
+
+var contenedor = document.getElementsByClassName("contendor")[0];
+var rosa = document.getElementById("rosa");
+var azul = document.getElementById("azul");
+var verde = document.getElementById("verde");
+var amarillo = document.getElementById("amarillo");
+
+contendor.style.position = "relative";
+contendor.style.background = "Darkgray";
+contendor.style.width = "600px";
+contendor.style.padding = "50px";
+
+azul.style.position = "absolute"
+azul.style.top = "250px";
+azul.style.right = "0";
